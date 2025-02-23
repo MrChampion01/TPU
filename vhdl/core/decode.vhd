3b941e5f245b30cd3d3403a14980e25be326a212
Chuck <146382740+MrChampion01@users.noreply.github.com>
2025-02-23 15:48:27 -0500

    Update decode.vhd

--- vhdl/core/decode.vhd
+++ vhdl/core/decode.vhd
@@ -1,93 +1,77 @@
 ----------------------------------------------------------------------------------
--- Project Name: TPU
--- Description: Decoder unit of TPU
+-- Company: WVU	
+-- Engineer: --Student: Charles Dunn
 -- 
--- Revision: 1
+-- Create Date:    12:05:36 11/21/2024 
+-- Design Name: dECODER
+-- Module Name:    decode - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: Simple Decoder Module which takes in instructions from a register
+-- and determines the appropiate operation to be perfomred from the first 3 alu op bits
+-- the next 3 bits for the register with the D if needed, the next bit just for a flag 
+-- for either high or low, the next 3 for the A register and 3 after that for the B register
+-- or those 6 bits plus the 2 remaing for an 8 bit value needing to be jumped
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-library work;
-use work.tpu_constants.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity decode is
     Port ( I_clk : in  STD_LOGIC;
+           I_dataInst : in  STD_LOGIC_VECTOR (15 downto 0);
            I_en : in  STD_LOGIC;
-           I_dataInst : in  STD_LOGIC_VECTOR (15 downto 0); -- Instruction to be decoded
-           O_selA : out  STD_LOGIC_VECTOR (2 downto 0);     -- Selection out for regA
-           O_selB : out  STD_LOGIC_VECTOR (2 downto 0);     -- Selection out for regB
-           O_selD : out  STD_LOGIC_VECTOR (2 downto 0);     -- Selection out for regD
-           O_dataIMM : out  STD_LOGIC_VECTOR (15 downto 0); -- Immediate value out
-           O_regDwe : out  STD_LOGIC;                       -- RegD wrtite enable
-           O_aluop : out  STD_LOGIC_VECTOR (4 downto 0)     -- ALU opcode
-    );
+           O_selA : out  STD_LOGIC_VECTOR (2 downto 0);
+           O_selB : out  STD_LOGIC_VECTOR (2 downto 0);
+           O_selD : out  STD_LOGIC_VECTOR (2 downto 0);
+           O_dataIMM : out  STD_LOGIC_VECTOR (15 downto 0);
+           O_regDwe : out  STD_LOGIC;
+           O_aluop : out  STD_LOGIC_VECTOR (4 downto 0));
 end decode;
 
 architecture Behavioral of decode is
 
 begin
 
-	process (I_clk, I_en)
-	begin
-		if rising_edge(I_clk) and I_en = '1' then
+process (I_clk)
+begin
+	if rising_edge(I_clk) and I_en ='1' then
 		
-			O_selA <= I_dataInst(IFO_RA_BEGIN downto IFO_RA_END);
-			O_selB <= I_dataInst(IFO_RB_BEGIN downto IFO_RB_END);
-			O_selD <= I_dataInst(IFO_RD_BEGIN downto IFO_RD_END);
-			O_dataIMM(7 downto 0) <= I_dataInst(IFO_IMM_BEGIN downto IFO_IMM_END);
-						  
-			O_aluop <= I_dataInst(IFO_OPCODE_BEGIN downto IFO_OPCODE_END) 
-			         & I_dataInst(IFO_F_LOC);
-	
-			case I_dataInst(IFO_OPCODE_BEGIN downto IFO_OPCODE_END) is
-				when OPCODE_WRITE => 
-					O_dataIMM(15 downto 8) <= I_dataInst(IFO_RD_BEGIN downto IFO_RD_END)
-											& I_dataInst(IFO_F2_BEGIN downto IFO_F2_END) & "000";
-					O_regDwe <= '0';
-				when OPCODE_JUMP => 
-					O_dataIMM(15 downto 8) <= "00000" & I_dataInst(IFO_RD_BEGIN downto IFO_RD_END);
-					O_regDwe <= '0';
-				when OPCODE_JUMPEQ => 
-					O_dataIMM(15 downto 8) <= I_dataInst(IFO_RD_BEGIN downto IFO_RD_END)
-											& I_dataInst(IFO_RB_BEGIN downto IFO_RB_END) & "00";
-					O_regDwe <= '0';
-					
-				when OPCODE_SPEC => 	-- special
-					
-					if I_dataInst(IFO_F_LOC) = '1' then
-						case I_dataInst(IFO_F2_BEGIN downto IFO_F2_END) is
-							
-							when OPCODE_SPEC_F2_GIEF =>
-								O_regDwe <= '1';
-							when OPCODE_SPEC_F2_BBI =>
-								O_regDwe <= '0';
-							when OPCODE_SPEC_F2_EI =>
-								O_regDwe <= '0';
-							when OPCODE_SPEC_F2_DI =>
-								O_regDwe <= '0';
-							when others =>
-						end case;
-					else
-						case I_dataInst(IFO_F2_BEGIN downto IFO_F2_END) is
-							when OPCODE_SPEC_F2_GETPC =>
-								O_regDwe <= '1';
-							when OPCODE_SPEC_F2_GETSTATUS =>
-								O_regDwe <= '1';
-							when OPCODE_SPEC_F2_INT =>
-								O_regDwe <= '0';
-							when others =>
-						end case;
-					end if;
-					
-					O_dataIMM(15 downto 8) <= I_dataInst(IFO_RD_BEGIN downto IFO_RD_END) & "00000";
-				when OPCODE_RES2 =>
-					O_regDwe <= '1';
-				when others =>
-					O_dataIMM(15 downto 8) <= X"00";
-					O_regDwe <= '1';
-			end case;
-		end if;
-	end process;
+		O_selA <= I_dataInst(7 downto 5);
+		O_selB <= I_dataInst(4 downto 2);
+		O_selD <= I_dataInst(11 downto 9);
+		O_dataIMM <= I_dataInst(7 downto 0) & I_dataInst(7 downto 0);
+		O_aluop <= I_dataInst(15 downto 12) & I_dataInst(8);
+		
+		case I_dataInst(15 downto 12) is
+			when "0111" => -- WRITE
+				O_regDwe <= '0';
+			when "1100" => -- JUMP
+				O_regDwe <= '0';
+			when "1101" => --JUMPEQ
+				O_regDwe <= '0';
+			when others =>
+				O_regDwe <= '1';
+		end case;
+	end if;
+end process;
+		
 
 end Behavioral;
 
